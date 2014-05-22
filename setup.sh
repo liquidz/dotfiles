6f4332df15649262ea0b4b40a4daafce13289ad7
uochan <liquidz.uo@gmail.com>
2014-05-22 09:07:18 +0000

    add setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+INSTALL_DIR=$HOME/.dotfiles
+
+git clone https://github.com/liquidz/dotfiles.git $INSTALL_DIR
+
+mkdir -p $HOME/.vim/bundle
+mkdir -p $HOME/.backup
+
+if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
+    git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
+fi
+
+ln -s $INSTALL_DIR/.vimrc $HOME/.vimrc
