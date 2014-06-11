93f5eff4d0f7984720c509399afbf732a97b3535
uochan <liquidz.uo@gmail.com>
2014-06-11 06:54:22 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+ * minimal setup
 ```
-curl https://raw.githubusercontent.com/liquidz/dotfiles/master/bin/setup_vim.sh | sh
+curl https://raw.githubusercontent.com/liquidz/dotfiles/master/bin/setup.sh | sh
+```
+ * full setup
+```
+curl https://raw.githubusercontent.com/liquidz/dotfiles/master/bin/setup.sh | env MODE=full sh
 ```
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,18 +1,58 @@
 #!/bin/sh
 
-INSTALL_DIR=$HOME/dotfiles
+#==================================================
+# sh ./setup.sh                    => minimal setup
+# env MODE=full sh ./setup.sh full => full setup
+#==================================================
 
+INSTALL_DIR=$HOME/dotfiles
+# colors {{{
+red=31
+green=32
+yellow=33
+blue=34
+# }}}
+cecho() { # {{{
+    color=$1
+    shift
+    echo -e "\033[${color}m$@\033[m"
+} # }}}
+
+recommended() {
+    cecho $green "Recommended operations:"
+    cat <<EOT
+ $ go get github.com/nsf/gocode
+EOT
+}
+
+if [ "${MODE}" == "" ]; then
+    MODE="minimal"
+fi
+
+cecho $green "Start: ${MODE} setup"
+
+cecho $yellow " * cloning dotfiles"
 if [ ! -e $INSTALL_DIR ]; then
     git clone https://github.com/liquidz/dotfiles.git $INSTALL_DIR
 fi
 
-ln -s $HOME/dotfiles/.vim $HOME/.vim
+cecho $yellow " * create symbolic links to dotfiles"
+for file in ".vimrc" ".tmux.conf" ".zshenv" ".zshrc"; do
+    ln -sfn $INSTALL_DIR/$file $HOME/$file
+done
 
-mkdir -p $HOME/.vim/bundle
-mkdir -p $HOME/.vim/backup
+if [ "${MODE}" == "full" ]; then
+    cecho $yellow " * initializing vim"
+    ln -sfn $INSTALL_DIR/.vim $HOME/.vim
+    mkdir -p $HOME/.vim/bundle
+    mkdir -p $HOME/.vim/backup
 
-if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
-    git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
+    cecho $yellow " * cloning neobundle"
+    if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
+        git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
+    fi
+
+    recommended
 fi
 
-ln -sf $INSTALL_DIR/.minimal_vimrc $HOME/.vimrc
+cecho $green "Done"
