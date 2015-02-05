ded07ab806ba8b93ec8383206a0e3ec4ef1e8406
liquidz <liquidz.uo@gmail.com>
2015-02-06 07:37:46 +0900

    update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,15 @@
 {:user
- {:plugins [[lein-cljfmt "0.1.5"]]
-  ;:dependencies [[slamhound "1.3.1"]]
+ {:plugins [
+            [lein-ancient "0.6.2"]
+            [jonase/eastwood "0.2.1"]
+            [lein-cloverage "1.0.2"]
+            [lein-kibit "0.0.8"]
+            [lein-cljfmt "0.1.5"]
+            ]
+  
+  :aliases {
+            "lint" ["eastwood"]
+            "check" ["kibit"]
+            "cover" ["cloverage"]
+            }
   }}
