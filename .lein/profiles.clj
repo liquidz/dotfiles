5be70f666cfd7ab359650970947f416769f2a229
liquidz <liquidz.uo@gmail.com>
2015-02-24 20:33:41 +0900

    update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,11 +5,17 @@
             [lein-cloverage "1.0.2"]
             [lein-kibit "0.0.8"]
             [lein-cljfmt "0.1.5"]
+            [lein-bikeshed "0.2.0"]
+            [codox "0.8.10"]
             ]
-  
+
   :aliases {
             "lint" ["eastwood"]
             "check" ["kibit"]
             "cover" ["cloverage"]
+            "omni" ["do"
+                    ["clean"]
+                    ;["with-profile" "production" "deps" ":tree"]
+                    ["ancient"] ["kibit"] ["bikeshed"]]
             }
   }}
--- .vim/conf.d/plugin.vim
+++ .vim/conf.d/plugin.vim
@@ -67,7 +67,7 @@ if has("unix")
     NeoBundle 'tpope/vim-classpath'
     NeoBundle 'typedclojure/vim-typedclojure'
     NeoBundle 'Shougo/unite-build'
-    NeoBundle 'venantius/vim-cljfmt'
+    "NeoBundle 'venantius/vim-cljfmt'
 endif
 
 "NeoBundle 'osyo-manga/vim-marching'
