bdb8e66c6c373a786c52af8f58617d6712ce4cd2
liquidz <liquidz.uo@gmail.com>
2015-03-15 10:39:47 +0900

    update

--- bin/setup.sh
+++ bin/setup.sh
@@ -18,11 +18,14 @@ cecho() { # {{{
     echo -e "\033[${color}m$@\033[m"
 } # }}}
 
+## git が未インストールなら終了
 if [ ! -x "`which git`" ]; then
     cecho $red "Error: git is not installed"
     exit 1
 fi
 
+## インストールモードの確認
+## full モード以外では dotfiles のシンボリックリンクのみ貼る
 echo -n "setup full mode? (y/N): "
 read MODE
 if [[ "$MODE" = "y" ]]; then
@@ -33,6 +36,7 @@ fi
 
 cecho $green "Start: ${MODE} setup"
 
+## dotfiles レポジトリの clone
 cecho $yellow " * cloning dotfiles"
 if [ ! -e $INSTALL_DIR ]; then
     git clone https://github.com/liquidz/dotfiles.git $INSTALL_DIR
@@ -40,18 +44,20 @@ else
     (cd $INSTALL_DIR && git pull origin master)
 fi
 
+## dotfiles のシンボリックリンクを貼る
 cecho $yellow " * create symbolic links to dotfiles"
 for file in ${DOT_FILES[@]}; do
     ln -sfn $INSTALL_DIR/$file $HOME/$file
 done
-
 cecho $yellow " * create symbolic links to .lein/profiles.clj"
 if [ ! -e "$HOME/.lein" ]; then
     mkdir -p ~/.lein
 fi
 ln -sfn $INSTALL_DIR/.lein/profiles.clj $HOME/.lein/profiles.clj
 
+## full モードなら全設定のセットアップ
 if [ "${MODE}" == "full" ]; then
+    ## vim の設定
     cecho $yellow " * initializing vim"
     ln -sfn $INSTALL_DIR/.vim $HOME/.vim
     mkdir -p $HOME/.vim/bundle
@@ -66,12 +72,14 @@ if [ "${MODE}" == "full" ]; then
         git clone https://github.com/Shougo/neobundle.vim.git $DIR
     fi
 
+    ## tmux の設定
     cecho $yellow " * cloning tpm"
     DIR="$HOME/.tmux/plugins/tpm"
     if [ ! -e $DIR ]; then
         git clone https://github.com/tmux-plugins/tpm $DIR
     fi
 
+    ## zsh の設定
     cecho $yellow " * cloning antigen"
     DIR="$HOME/src/github.com/zsh-users/antigen"
     if [ ! -e $DIR ]; then
@@ -89,6 +97,7 @@ if [ "${MODE}" == "full" ]; then
     cecho $yellow " * git config"
     git config --global ghq.root ~/src
     git config --global github.user liquidz
+    git config --global push.default current
 fi
 
 cecho $green "Done"
