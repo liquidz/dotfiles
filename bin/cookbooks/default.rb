e15ff4fb767397f1abed0fad443043bed9ee0d46
liquidz <liquidz.uo@gmail.com>
2016-11-21 15:01:48 +0000

    update

--- bin/cookbooks/default.rb
+++ bin/cookbooks/default.rb
@@ -1,27 +1,84 @@
-home = ENV['HOME']
-install_dir = "#{home}/src/github.com/liquidz/dotfiles"
-
-git install_dir do
-  repository 'https://github.com/liquidz/dotfiles.git'
-end
-
-# 必要なディレクトリを作成
-%w(
-  .lein
-).each do |dir|
-  directory "#{home}/#{dir}"
-end
-
-## dotfiles のシンボリックリンクを貼る
-%w(
-  .vimrc .tmux.conf
-  .zshenv .zshrc .zshrc.antigen
-  .ctags .gemrc .rubocop.yml
-  .gitconfig.common .w3m .cheatrc
-  .lein/profiles.clj
-).each do |file|
-  link "#{home}/#{file}" do
-    to "#{install_dir}/#{file}"
+define :curl, path: nil, url: nil do # {{{
+  execute "curl #{params[:name]}" do
+    command <<-EOT
+      curl -sfLo #{params[:path]} #{params[:url]}
+    EOT
+    not_if "test -e #{params[:path]}"
   end
+end # }}}
+
+define :dotfiles do
+  home = params[:name]
+  install_dir = "#{home}/src/github.com/liquidz/dotfiles"
+
+  # git clone {{{
+
+  git install_dir do
+    repository 'https://github.com/liquidz/dotfiles.git'
+  end
+
+  # }}}
+  # 必要なディレクトリを作成 {{{
+
+  %w( .tags .lein ).each do |dir|
+    directory "#{home}/#{dir}"
+  end
+
+  # }}}
+  # dotfiles のシンボリックリンクを貼る {{{
+
+  %w(
+    .vimrc .tmux.conf
+    .zshenv .zshrc .zshrc.antigen
+    .ctags .gemrc .rubocop.yml
+    .gitconfig.common .w3m .cheatrc
+    .lein/profiles.clj
+  ).each do |file|
+    link "#{home}/#{file}" do
+      to "#{install_dir}/#{file}"
+    end
+  end
+
+  # }}}
+  # vim の設定 {{{
+
+  link "#{home}/.vim" do
+    to "#{install_dir}/.vim"
+  end
+
+  %w( .vim/backup .vim/memo .vim/autoload ).each do |dir|
+    directory "#{home}/#{dir}"
+  end
+
+  curl 'vim-plug' do
+    path "#{home}/.vim/autoload/plug.vim"
+    url "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  end
+
+  # }}}
+  # zsh の設定 {{{
+
+  link "#{home}/.zsh" do
+    to "#{install_dir}/.zsh"
+  end
+
+  {
+    '_git'                => 'git-completion.zsh',
+    'git-completion.bash' => 'git-completion.bash',
+  }.each do |name, file|
+    curl name do
+      path "#{home}/.zsh/#{name}"
+      url "https://raw.githubusercontent.com/git/git/master/contrib/completion/#{file}"
+    end
+  end
+
+  git "#{home}/src/github.com/zsh-users/antigen" do
+    repository 'https://github.com/zsh-users/antigen'
+  end
+
+  # }}}
 end
 
+dotfiles ENV['HOME']
+
+# vim:foldlevel=0
