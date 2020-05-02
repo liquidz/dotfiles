74bf66166ae90ad86912a1fc2921520828dddabc
liquidz <liquidz.uo@gmail.com>
2020-05-03 08:09:54 +0900

    Update deps.edn template

--- templates/deps.edn/setup.sh
+++ templates/deps.edn/setup.sh
@@ -9,4 +9,8 @@ TEST_DIR=test/$DIR_NAME
 mv src/dummy $SRC_DIR
 mv test/dummy $TEST_DIR
 
-find . -type f | xargs sed -i -e "s/__dummy__/$PROJECT_NAME/g"
+if [ "$(uname)" == 'Darwin' ]; then
+    find . -type f | xargs sed -i '' -e "s/__dummy__/$PROJECT_NAME/g"
+else
+    find . -type f | xargs sed -i -e "s/__dummy__/$PROJECT_NAME/g"
+fi
